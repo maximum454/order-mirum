e8f61afd0f83f843c4ebbf672d00831af1b5cd1b
maxim <maximum454@gmail.com>
2019-05-17 15:27:38 +0600

    проект

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/order.js',
+    output: {
+        filename: 'order.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "./"
+    },
+    devtool: "source-map",
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: 'env'
+                    }
+                }
+            },
+
+            {
+                test: /\.(sass|scss)$/,
+                use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png)$/,
+                exclude: /fonts/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            useRelativePath: true
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 80,
+
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/,
+                exclude: /fonts/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'img/',
+                            name: '[name].[ext]',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                exclude: /img/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            //useRelativePath: true,
+                            outputPath: 'fonts/',
+                            name: '[name].[ext]'
+                        },
+                    },
+                ],
+            }
+
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'order.css',
+        }),
+    ],
+
+};
